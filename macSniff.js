c8338f7765406da6a2da9ca95579b602feb39c13
JAIR CARVALHO <jaircarvalho@MacBook-Pro-de-JAIR.local>
2017-05-12 12:17:43 -0300

    First commit

--- macSniff.js
+++ macSniff.js
@@ -1,5 +1,7 @@
 // This is a simple code to execute read the MAC Adresses and BSSI of 
 // wifi stations and access points packages that are captured by the network interface
+// references:
+// http://www.tcpdump.org/manpages/pcap-filter.7.txt
 
 var pcap = require('pcap');
 pcap_session = pcap.createSession(process.argv[2], process.argv[3]);
@@ -7,6 +9,7 @@ pcap_session = pcap.createSession(process.argv[2], process.argv[3]);
 
 pcap_session.on('packet', function (raw_packet) {
     // do some stuff with a raw packet
+    // http://www.tcpdump.org/manpages/pcap-filter.7.txt -> Doc for packages types/subtypes 
     // We need to be carefull with the diferent types and subtypes of the packages
     // The main issue is that some of these types/subtypes don't have the parse routine created
     // In the current code what can happen is that the program crashes
