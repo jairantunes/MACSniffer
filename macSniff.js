bc6b5672d85f400eee07fd6de89b5bb178a23078
Jair Antunes <jair.a.carvalho.jr@gmail.com>
2017-05-25 11:37:17 -0300

    add timestamp

--- macSniff.js
+++ macSniff.js
@@ -26,7 +26,7 @@ pcap_session.on('packet', function (raw_packet) {
     // and/or adjust our own parser routine
     var packet = pcap.decode.packet(raw_packet);
 //    console.log("strength: %s ", packet.signalStrength);
-    console.log("PWR: " + packet.payload.signalStrength + " || shost: " + packet.payload.ieee802_11Frame.shost + " || dhost: " + packet.payload.ieee802_11Frame.dhost + " || bssid: " + packet.payload.ieee802_11Frame.bssid);
+    console.log( Date.now() + " PWR: " + packet.payload.signalStrength + " || shost: " + packet.payload.ieee802_11Frame.shost + " || dhost: " + packet.payload.ieee802_11Frame.dhost + " || bssid: " + packet.payload.ieee802_11Frame.bssid);
 //    console.log(packet);
 }); 
 
