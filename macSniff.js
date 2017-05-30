a46afd4984e1141c0e35a65cb3c5a5f8453742ca
Jair Antunes <jair.a.carvalho.jr@gmail.com>
2017-05-30 01:58:28 +0000

    adding info about the packet

--- macSniff.js
+++ macSniff.js
@@ -17,6 +17,7 @@ pcap_session = pcap.createSession(process.argv[2], process.argv[3]);
 
 
 pcap_session.on('packet', function (raw_packet) {
+
     // do some stuff with a raw packet
     // http://www.tcpdump.org/manpages/pcap-filter.7.txt -> Doc for packages types/subtypes 
     // We need to be carefull with the diferent types and subtypes of the packages
@@ -24,9 +25,16 @@ pcap_session.on('packet', function (raw_packet) {
     // In the current code what can happen is that the program crashes
     // So we need create the proper error handling mechanism 
     // and/or adjust our own parser routine
+
     var packet = pcap.decode.packet(raw_packet);
-//    console.log("strength: %s ", packet.signalStrength);
-    console.log(" PWR: " + packet.payload.signalStrength + " || shost: " + packet.payload.ieee802_11Frame.shost + " || dhost: " + packet.payload.ieee802_11Frame.dhost + " || bssid: " + packet.payload.ieee802_11Frame.bssid);
-//    console.log(packet);
+
+    console.log( " time: "      + packet.payload.tsft + 
+                 " signal: "    + packet.payload.signalStrength + 
+                 " ethertype: " + packet.payload.ethertype +
+                 " emitter: "   + packet.payload.ieee802_11Frame.emitter +
+                 " shost: "     + packet.payload.ieee802_11Frame.shost + 
+                 " dhost: "     + packet.payload.ieee802_11Frame.dhost + 
+                 " bssid: "     + packet.payload.ieee802_11Frame.bssid);
+
 }); 
 
